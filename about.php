f388bcefb8bdbc3c3bfbadb518c7c41105e420c9
Obaidul Haque <30920776+obaidul99@users.noreply.github.com>
2026-01-07 12:19:58 +0600

    Update about.php

--- about.php
+++ about.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>About Us - MediCare Hospital</title>
+    <title>About Us - Al Mutmainnah Maa O Shishu Haspatal</title>
     
     <!-- Bootstrap 5 CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
